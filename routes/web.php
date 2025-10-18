a04b84db6e32dfdfeaad1f3f9fd5ac2a6447ecd6
Lance <xciden@gmail.com>
2025-10-18 21:34:16 +0800

    web route file

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('web')->group(function () {
+    // this is the route that will be used to redirect to Singpass login page
+    Route::get(
+        config('singpass-myinfo.authentication_endpoint_url'),
+        config('singpass-myinfo.authentication_endpoint_controller')
+    )->name('singpass.login');
+
+    // this is the route that will be called when Singpass redirects back after authentication
+    Route::get(
+        config('singpass-myinfo.callback_endpoint_url'),
+        config('singpass-myinfo.callback_endpoint_controller')
+    )->name('singpass.callback');
+
+    // this is the url that Singpass will call to retrieve your public jwks for signing and encryption
+    Route::get(
+        config('singpass-myinfo.jwks_endpoint_url'),
+        config('singpass-myinfo.jwks_endpoint_controller')
+    )->name('singpass.jwks');
+});
